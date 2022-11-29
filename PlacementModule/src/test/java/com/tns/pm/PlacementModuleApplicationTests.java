8be8bb1f0f5b77164b41925358b5bca35062dd65
Amrutha gowda <Amrutha gowda@MSI>
2022-11-29 15:40:18 +0530

    project commit

---
+++ PlacementModule/src/test/java/com/tns/pm/PlacementModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.pm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
